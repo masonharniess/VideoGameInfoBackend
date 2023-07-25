f16c801a82a55112ec9737263dab52c9951e513b
Mason Harniess <maseharniess@outlook.com>
2023-07-25 16:52:05 +0100

    feat: add DevelopersController and DevelopersWithoutGamesDto

---
+++ VideoGameInfo.API/Controllers/DevelopersController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VideoGameInfo.API.Models;
+
+namespace VideoGameInfo.API.Controllers
+{
+
+    [ApiController]
+    [Route("api/developers")]
+    public class DevelopersController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<DevelopersWithoutGamesDto>>> GetDevelopers()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ VideoGameInfo.API/Models/DevelopersWithoutGamesDto.cs
@@ -0,0 +1,15 @@
+﻿namespace VideoGameInfo.API.Models
+{
+    public class DevelopersWithoutGamesDto
+    {
+        public DevelopersWithoutGamesDto(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public int Id { get; }
+        public string Name { get; }
+        public string? Description { get; }
+    }
+}
