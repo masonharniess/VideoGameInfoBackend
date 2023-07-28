bfdc8d86d1deccce59a41c32b689e57390f00913
Mason Harniess <maseharniess@outlook.com>
2023-07-28 11:51:04 +0100

    feat: add DeveloperProfile

---
+++ VideoGameInfo.API/Profiles/DeveloperProfile.cs
@@ -0,0 +1,10 @@
+﻿namespace VideoGameInfo.API.Profiles
+{
+    public class DeveloperProfile
+    {
+        public DeveloperProfile() { 
+            CreateMap<Entities.Developer, Models.DevelopersWithoutGamesDto>();
+            CreateMap<Entities.Developer, Models.DeveloperDto>();
+        }
+    }
+}
